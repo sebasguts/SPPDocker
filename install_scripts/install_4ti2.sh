a2dd89d20e0372dca7518aceeec0ae0dcd3a9064
Max Horn <max@quendi.de>
2015-11-05 12:17:36 +0100

    Avoid repeating version number
    This simplifies updates to newer versions

--- install_scripts/install_4ti2.sh
+++ install_scripts/install_4ti2.sh
@@ -4,17 +4,19 @@ echo "installing 4ti2"
 
 number_cores=$(cat /proc/cpuinfo | grep processor | wc -l)
 
+VERSION=1.6.6
+
 cd /tmp
-wget http://www.4ti2.de/version_1.6.6/4ti2-1.6.6.tar.gz
-tar -xf 4ti2-1.6.6.tar.gz
-cd 4ti2-1.6.6
+wget http://www.4ti2.de/version_$VERSION/4ti2-$VERSION.tar.gz
+tar -xf 4ti2-$VERSION.tar.gz
+cd 4ti2-$VERSION
 ./configure --with-gmp=$1
 make -j${number_cores}
 sudo make install
 cd /tmp
-rm -rf 4ti2-1.6.6
-tar -xf 4ti2-1.6.6.tar.gz
-cd 4ti2-1.6.6
+rm -rf 4ti2-$VERSION
+tar -xf 4ti2-$VERSION.tar.gz
+cd 4ti2-$VERSION
 mkdir $2
 ./configure --prefix=$2 --enable-shared --with-gmp=$1
 make -j${number_cores}
