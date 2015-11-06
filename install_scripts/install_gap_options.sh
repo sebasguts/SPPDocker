ee407ff344d1a1b0eb2a9e44ce14846a2650fc11
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2015-11-06 14:37:49 +0100

    Made files writable in gap options

--- install_scripts/install_gap_options.sh
+++ install_scripts/install_gap_options.sh
@@ -6,17 +6,19 @@ number_cores=$(cat /proc/cpuinfo | grep processor | wc -l)
 
 mkdir -p /opt/gap/local/pkg
 
-sudo cat > /usr/bin/gap <<EOF
+sudo touch /usr/bin/gap
+sudo chmod 0755 /usr/bin/gap
+cat > /usr/bin/gap <<EOF
 #!/bin/bash
 /opt/gap/bin/gap.sh -l "/opt/gap/local;/opt/gap" "\$@"
 EOF
-sudo chmod 0755 /usr/bin/gap
 
-sudo cat > /usr/bin/gapL <<EOF
+sudo touch /usr/bin/gapL
+sudo chmod 0755 /usr/bin/gapL
+cat > /usr/bin/gapL <<EOF
 #!/bin/bash
 /opt/gap/bin/gap.sh -l "/opt/gap/local;/opt/gap" -L /opt/gap/bin/wsgap4 "\$@"
 EOF
-sudo chmod 0755 /usr/bin/gapL
 
 mkdir /home/spp/.gap
 cat > /home/spp/.gap/gap.ini <<EOF
